f22a4fe08d46edd5aa26aa373ae70a678da4a359
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2023-06-01 23:19:49 -0400

    use snake case formatting, move event description to separate file

--- custom_components/petkit/const.py
+++ custom_components/petkit/const.py
@@ -35,30 +35,30 @@ PETKIT_ERRORS = (
 
 # Dictionaries and lists.
 
-LIGHT_BRIGHTNESS_OPTIONS = ['Low', 'Medium', 'High']
+LIGHT_BRIGHTNESS_OPTIONS = ['low', 'medium', 'high']
 
 LIGHT_BRIGHTNESS_COMMAND = {
-    W5Command.LIGHTLOW: "Low",
-    W5Command.LIGHTMEDIUM: "Medium",
-    W5Command.LIGHTHIGH: "High"
+    W5Command.LIGHT_LOW: "low",
+    W5Command.LIGHT_MEDIUM: "medium",
+    W5Command.LIGHT_HIGH: "high"
 }
 
 LIGHT_BRIGHTNESS_NAMED = {
-    1: 'Low',
-    2: 'Medium',
-    3: 'High'
+    1: 'low',
+    2: 'medium',
+    3: 'high'
 }
 
-WF_MODE_OPTIONS = ['Normal', 'Smart']
+WF_MODE_OPTIONS = ['normal', 'smart']
 
 WF_MODE_NAMED = {
-    1: 'Normal',
-    2: 'Smart'
+    1: 'normal',
+    2: 'smart'
 }
 
 WF_MODE_COMMAND = {
-    W5Command.NORMAL: "Normal",
-    W5Command.SMART: "Smart"
+    W5Command.NORMAL: "normal",
+    W5Command.SMART: "smart"
 }
 
 WATER_FOUNTAINS = {
@@ -106,9 +106,9 @@ MINI_FEEDER_MANUAL_FEED_OPTIONS = [
 ]
 
 LITTER_TYPE_NAMED = {
-    1: 'Bentonite',
-    2: 'Tofu',
-    3: 'Mixed'
+    1: 'bentonite',
+    2: 'tofu',
+    3: 'mixed'
 }
 
 MANUAL_FEED_NAMED = {
@@ -124,104 +124,3 @@ MANUAL_FEED_NAMED = {
     45: '9/20th Cup (45g)',
     50: '1/2 Cup (50g)'
 }
-
-VALID_EVENT_TYPES = [5, 6, 7, 8, 10]
-EVENT_TYPE_NAMED = {
-    5: 'Cleaning Completed',
-    6: 'Dumping Over',
-    7: 'Reset Over',
-    8: 'Spray Over',
-    10: 'Pet Out',
-}
-
-# Event Type --> Result --> Reason --> Optional(Error)
-EVENT_DESCRIPTION = {
-    5: {
-        0: {
-            0: 'Auto cleaning completed',
-            1: 'Periodic cleaning completed',
-            2: 'Manual cleaning completed',
-            3: 'Manual cleaning completed',
-        },
-        1: {
-            0: 'Automatic cleaning terminated',
-            1: 'Periodic cleaning terminated',
-            2: 'Manual cleaning terminated',
-            3: 'Manual cleaning terminated',
-        },
-        2: {
-            0: {
-                'full': 'Automatic cleaning failed, waste collection bin is full, please empty promptly',
-                'hallL': 'Automatic cleaning failure, the cylinder is not properly locked in place, please check',
-                'hallT': 'Automatic cleaning failure, the litter box\'s upper cupper cover is not placed properly, please check',
-            },
-            1: {
-                'full': 'Scheduled cleaning failed, waste collection bin is full, please empty promptly',
-                'hallL': 'Scheduled cleaning failure, the cylinder is not properly locked in place, please check',
-                'hallT': 'Scheduled cleaning failure, the litter box\'s upper cupper cover is not placed properly, please check',
-            },
-            2: {
-                'full': 'Manual cleaning failed, waste collection bin is full, please empty promptly',
-                'hallL': 'Manual cleaning failure, the cylinder is not properly locked in place, please check',
-                'hallT': 'Manual cleaning failure, the litter box\'s upper cupper cover is not placed properly, please check',
-            },
-            3: {
-                'full': 'Manual cleaning failed, waste collection bin is full, please empty promptly',
-                'hallL': 'Manual cleaning failure, the cylinder is not properly locked in place, please check',
-                'hallT': 'Manual cleaning failure, the litter box\'s upper cupper cover is not placed properly, please check',
-            },
-        },
-        3: {
-            0: 'Automatic cleaning cancelled, device in operation',
-            1: 'Periodic cleaning cancelled, device in operation',
-            2: 'Manual cleaning cancelled, device in operation',
-            3: 'Manual cleaning cancelled, device in operation',
-        },
-        4: {
-            0: 'Kitten mode is enabled, auto cleaning is canceled',
-            1: 'Kitten mode is enabled, periodically cleaning is canceled',
-        },
-    },
-    6: {
-        0: 'Cat litter empty completed',
-        1: 'Cat litter empty terminated',
-        2: {
-            'full': 'Cat litter empty failed, waste collection bin is full, please empty promptly',
-            'hallL': 'Cat litter empty failure, the cylinder is not properly locked in place, please check',
-            'hallT': 'Cat litter empty failure, the litter box\'s cupper cover is not placed properly, please check',
-        },
-    },
-    7: {
-        0: 'Device reset completed',
-        1: 'Device reset terminated',
-        2: {
-            'full': 'Device reset failed, waste collection bin is full, please empty promptly',
-            'hallL': 'Device reset failure, the cylinder is not properly locked in place, please check',
-            'hallT': 'Device reset failure, the litter box\'s cupper cover is not placed properly, please check',
-        },
-    },
-    8: {
-        0: {
-            0: 'Deodorant finished',
-            1: 'Periodic odor removal completed',
-            2: 'Manual odor removal completed',
-            3: 'Manual odor removal completed',
-        },
-        1: {
-            0: 'Deodorant finished, not enough purifying liquid, please refill in time',
-            1: 'Periodic odor removal completed, not enough purifying liquid, please refill in time',
-            2: 'Manual odor removal completed, not enough purifying liquid, please refill in time',
-            3: 'Manual odor removal completed, not enough purifying liquid, please refill in time',
-        },
-        2: {
-            0: 'Automatic odor removal failed, odor eliminator error',
-            1: 'Periodic odor removal failure, odor eliminator malfunction',
-            2: 'Manual odor removal failure, odor eliminator malfunction',
-            3: 'Manual odor removal failure, odor eliminator malfunction',
-        },
-    },
-}    
-    
-    
-    
-    
