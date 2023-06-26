16abe8deb078fe4794a4a6808e35368684742311
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2023-06-26 16:50:03 -0400

    migrate config entry, reload on options change

--- custom_components/petkit/__init__.py
+++ custom_components/petkit/__init__.py
@@ -2,10 +2,10 @@
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_EMAIL
+from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, LOGGER, PLATFORMS
+from .const import ASIA_ACCOUNT, DOMAIN, LOGGER, PETKIT_COORDINATOR, PLATFORMS, POLLING_INTERVAL, UPDATE_LISTENER
 from .coordinator import PetKitDataUpdateCoordinator
 from .util import async_validate_api, NoDevicesError
 
@@ -15,9 +15,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     coordinator = PetKitDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
+        PETKIT_COORDINATOR: coordinator
+    }
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    update_listener = entry.add_update_listener(async_update_options)
+    hass.data[DOMAIN][entry.entry_id][UPDATE_LISTENER] = update_listener
 
     return True
 
@@ -30,3 +35,32 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         if not hass.data[DOMAIN]:
             del hass.data[DOMAIN]
     return unload_ok
+
+async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Migrate old entry."""
+
+    if entry.version == 1:
+        email = entry.data[CONF_EMAIL]
+        password = entry.data[CONF_PASSWORD]
+
+        LOGGER.debug('Migrating PetKit config entry')
+        entry.version = 2
+
+        hass.config_entries.async_update_entry(
+            entry,
+            data={
+                CONF_EMAIL: email,
+                CONF_PASSWORD: password,
+            },
+            options={
+                ASIA_ACCOUNT: False,
+                POLLING_INTERVAL: 120,
+            },
+        )
+
+    return True
+
+async def async_update_options(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """ Update options. """
+
+    await hass.config_entries.async_reload(entry.entry_id)
