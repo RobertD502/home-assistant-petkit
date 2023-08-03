9aa6a5a02aded4265db582aa9744efba1b78eb7b
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2023-08-03 15:25:20 -0400

    add region parameter to PetKit client

--- custom_components/petkit/coordinator.py
+++ custom_components/petkit/coordinator.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from datetime import timedelta
 
 from petkitaio import PetKitClient
-from petkitaio.exceptions import AuthError, PetKitError, ServerError
+from petkitaio.exceptions import AuthError, PetKitError, RegionError, ServerError
 from petkitaio.model import PetKitData
 
 
@@ -15,7 +15,7 @@ from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import ASIA_ACCOUNT, CHINA_ACCOUNT, DOMAIN, LOGGER, POLLING_INTERVAL, TIMEOUT
+from .const import DOMAIN, LOGGER, POLLING_INTERVAL, REGION, TIMEOUT
 
 
 class PetKitDataUpdateCoordinator(DataUpdateCoordinator):
@@ -26,20 +26,22 @@ class PetKitDataUpdateCoordinator(DataUpdateCoordinator):
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
         """Initialize the PetKit coordinator."""
 
-        self.client = PetKitClient(
-            entry.data[CONF_EMAIL],
-            entry.data[CONF_PASSWORD],
-            session=async_get_clientsession(hass),
-            asia_account=entry.options[ASIA_ACCOUNT],
-            china_account=entry.options[CHINA_ACCOUNT],
-            timeout=TIMEOUT,
-        )
-        super().__init__(
-            hass,
-            LOGGER,
-            name=DOMAIN,
-            update_interval=timedelta(seconds=entry.options[POLLING_INTERVAL]),
-        )
+        try:
+            self.client = PetKitClient(
+                entry.data[CONF_EMAIL],
+                entry.data[CONF_PASSWORD],
+                session=async_get_clientsession(hass),
+                region=entry.options[REGION],
+                timeout=TIMEOUT,
+            )
+            super().__init__(
+                hass,
+                LOGGER,
+                name=DOMAIN,
+                update_interval=timedelta(seconds=entry.options[POLLING_INTERVAL]),
+            )
+        except RegionError as error:
+            raise ConfigEntryAuthFailed(error) from error
 
     async def _async_update_data(self) -> PetKitData:
         """Fetch data from PetKit."""
@@ -47,8 +49,9 @@ class PetKitDataUpdateCoordinator(DataUpdateCoordinator):
         try:
             data = await self.client.get_petkit_data()
             LOGGER.debug(f'Found the following PetKit devices/pets: {data}')
-        except AuthError as error:
+        except (AuthError, RegionError) as error:
             raise ConfigEntryAuthFailed(error) from error
         except (ServerError, PetKitError) as error:
             raise UpdateFailed(error) from error
-        return data
+        else:
+            return data
