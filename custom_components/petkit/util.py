4163ec6ea81e9d507a242c5809ef0446031e55b2
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2023-08-03 15:27:44 -0400

    add region parameter to PetKit client

--- custom_components/petkit/util.py
+++ custom_components/petkit/util.py
@@ -5,7 +5,7 @@ from typing import Any
 import async_timeout
 
 from petkitaio import PetKitClient
-from petkitaio.exceptions import AccountTypeError, AuthError, PetKitError, ServerError
+from petkitaio.exceptions import AuthError, PetKitError, RegionError, ServerError
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
@@ -13,15 +13,14 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import LOGGER, PETKIT_ERRORS, TIMEOUT
 
 
-async def async_validate_api(hass: HomeAssistant, email: str, password: str, asia_account: bool, china_account: bool) -> bool:
+async def async_validate_api(hass: HomeAssistant, email: str, password: str, region: str) -> bool:
     """Get data from API."""
 
     client = PetKitClient(
         email,
         password,
         session=async_get_clientsession(hass),
-        asia_account=asia_account,
-        china_account=china_account,
+        region=region,
         timeout=TIMEOUT,
     )
 
@@ -34,9 +33,9 @@ async def async_validate_api(hass: HomeAssistant, email: str, password: str, asi
     except ServerError as err:
         LOGGER.error(f'PetKit servers are busy.Please try again later.')
         raise ServerError(err)
-    except AccountTypeError as err:
+    except RegionError as err:
         LOGGER.error(f'{err}')
-        raise AccountTypeError(err)
+        raise RegionError(err)
     except PetKitError as err:
         LOGGER.error(f'Unknown PetKit Error: {err}')
         raise PetKitError(err)
