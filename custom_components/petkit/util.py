853380485984cfea3898a02a8e6b52d7330e0bff
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2024-11-04 16:26:46 -0500

    use renamed function when device_query used during config flow

--- custom_components/petkit/util.py
+++ custom_components/petkit/util.py
@@ -30,7 +30,7 @@ async def async_validate_api(hass: HomeAssistant, email: str, password: str, reg
     )
     try:
         async with async_timeout.timeout(TIMEOUT):
-            devices_query = await client.get_device_roster()
+            devices_query = await client.get_device_rosters()
     except AuthError as err:
         LOGGER.error(f'Could not authenticate on PetKit servers: {err}')
         raise AuthError(err)
@@ -51,9 +51,10 @@ async def async_validate_api(hass: HomeAssistant, email: str, password: str, reg
         LOGGER.error(f'Failed to get information from PetKit servers: {err}')
         raise ConnectionError from err
 
-    devices: dict[str, Any] = devices_query['result']['devices']
+    devices = sum(len(value['result']['devices']) for value in devices_query.values())
 
-    if not devices:
+
+    if devices == 0:
         LOGGER.error("Could not retrieve any devices from PetKit servers")
         raise NoDevicesError
     return True
