fb5de32e7d0e20b0baad4fe8fdbc5e61cdd53305
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2025-01-05 18:01:35 -0500

    add fast poll and use_ble_relay setting

--- custom_components/petkit/coordinator.py
+++ custom_components/petkit/coordinator.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from datetime import timedelta
+import json
 
 from petkitaio import PetKitClient
 from petkitaio.exceptions import AuthError, PetKitError, RegionError, ServerError
@@ -15,7 +16,15 @@ from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import DOMAIN, LOGGER, POLLING_INTERVAL, REGION, TIMEOUT, TIMEZONE
+from .const import (
+    DOMAIN,
+    LOGGER,
+    POLLING_INTERVAL,
+    REGION,
+    TIMEOUT,
+    TIMEZONE,
+    USE_BLE_RELAY,
+)
 
 
 class PetKitDataUpdateCoordinator(DataUpdateCoordinator):
@@ -30,6 +39,7 @@ class PetKitDataUpdateCoordinator(DataUpdateCoordinator):
             tz = None
         else:
             tz = entry.options[TIMEZONE]
+        self.fast_poll_expiration = None
         try:
             self.client = PetKitClient(
                 entry.data[CONF_EMAIL],
@@ -39,6 +49,7 @@ class PetKitDataUpdateCoordinator(DataUpdateCoordinator):
                 timezone=tz,
                 timeout=TIMEOUT,
             )
+            self.client.use_ble_relay = entry.options[USE_BLE_RELAY]
             super().__init__(
                 hass,
                 LOGGER,
@@ -53,7 +64,11 @@ class PetKitDataUpdateCoordinator(DataUpdateCoordinator):
 
         try:
             data = await self.client.get_petkit_data()
-            LOGGER.debug(f'Found the following PetKit devices/pets: {data}')
+            nl = '\n'
+            LOGGER.debug(f'Found the following PetKit devices/pets: {nl}{json.dumps(data, default=vars, indent=4)}')
+        except TypeError:
+            LOGGER.debug('Encountered TypeError while formatting returned PetKit devices for logging.')
+            return data
         except (AuthError, RegionError) as error:
             raise ConfigEntryAuthFailed(error) from error
         except (ServerError, PetKitError) as error:
